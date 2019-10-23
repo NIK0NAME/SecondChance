1addc77d92510a2170f8ac929caedb64a6a7dead
niko97killer <nikolayzabaleta@hotmail.com>
2019-10-23 08:44:14 +0200

    Haciendo limpieza por aka

--- SecondChances/Global.asax.cs
+++ SecondChances/Global.asax.cs
@@ -17,5 +17,21 @@ namespace SecondChances
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            //Funcion que se ejecuta cuando se crea la session
+            Session["Usuario"] = "Anonimo";
+            Session["Id"] = 0;
+            Session["Autenticacion"] = false;
+
+            //Guardamos el numero de usuarios conectados a la app
+            Application["Users_connected"] = int.Parse(Application["Users_connected"].ToString()) + 1;
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Exepciones.htm");
+        }
     }
 }
