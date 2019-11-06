c9114060ffd40908962a4a22912ea79e0a38875e
niko97killer <nikolayzabaleta@hotmail.com>
2019-11-06 10:11:18 +0100

    merged

--- SecondChances/Login.aspx.cs
+++ SecondChances/Login.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -25,27 +26,38 @@ namespace SecondChances
 
         public void validarDatos()
         {
-            //Los datos vienen validados desde el cliente
-            String user = data_user.Text;
-            String pass = data_pass.Text;
-
-            //Preparamos la query 
-            string sql = "SELECT * FROM users WHERE username = '" + user + "' and pass = '" + pass + "'";
-
-            SqlCommand cmd = new SqlCommand(sql, conn);
-            //cmd.CommandText = sql;
-            cmd.CommandType = System.Data.CommandType.Text;
-            //cmd.Connection = conn;
-            
-            SqlDataReader reader = cmd.ExecuteReader();
-
-            if(reader.HasRows)
+            try
             {
-                //El usuario se ha logueado correctamente
-                Response.Redirect("Default.aspx");
-            }else
+                conn = new SqlConnection(ConfigurationManager.ConnectionStrings["mi_conexion"].ConnectionString);
+                conn.Open();
+                //Los datos vienen validados desde el cliente
+                String user = data_user.Text;
+                String pass = data_pass.Text;
+
+                //Preparamos la query 
+                string sql = "SELECT * FROM users WHERE username = '" + user + "' and pass = '" + pass + "'";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                //cmd.CommandText = sql;
+                cmd.CommandType = System.Data.CommandType.Text;
+                //cmd.Connection = conn;
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    //El usuario se ha logueado correctamente
+                    Response.Redirect("Default.aspx");
+                }
+                else
+                {
+                    //Algo no rulo bene
+                    //this.Master.thorwAlertDude("El jusuario no eta");
+                }
+            }
+            catch(Exception ex)
             {
-                //Algo no rulo bene
+                //this.Master.thorwAlertDude(ex.ToString());
             }
         }
     }
