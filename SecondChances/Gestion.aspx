5bba4b313fe47b5aefe3da9bf12d7906c3aae24d
Niko <nikolayzabaleta@hotmail.com>
2019-11-13 19:30:22 +0100

    seccion gestion creada

---
+++ SecondChances/Gestion.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="Gestion" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Gestion.aspx.cs" Inherits="SecondChances.Gestion" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="gestion_container">
+        
+    </div>
+</asp:Content>
---
+++ SecondChances/Gestion.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SecondChances
+{
+    public partial class Gestion : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["user"] == null || Int32.Parse(Session["admin"].ToString()) == 0)
+            {
+                Response.Redirect("Login.aspx");
+            }
+            else
+            {
+                
+            }
+        }
+    }
+}
